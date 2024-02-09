9e0b1c4f472311c05ae41616606324a2fdac99ea
Michael J Erwin <michaeljameserwin@gmail.com>
2024-02-09 15:15:02 -0500

    Add darwin x86_64

--- libc/libc.ml
+++ libc/libc.ml
@@ -1,6 +1,9 @@
 module Impl = Libc_aarch64_apple_darwin
 [@@config all (target_os = "macos", target_arch = "aarch64")]
 
+module Impl = Libc_aarch64_apple_darwin
+[@@config all (target_os = "macos", target_arch = "x86_64")]
+
 module Impl = Libc_x86_64_linux_gnu
 [@@config all (target_os = "linux", target_arch = "x86_64", target_env = "gnu")]
 
