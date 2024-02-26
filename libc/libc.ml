feeec442b744c25357d6cb2182cf80cb28526632
Leandro Ostera <leandro@abstractmachines.dev>
2024-02-26 22:56:03 +0100

    feat: simplified imports

--- libc/libc.ml
+++ libc/libc.ml
@@ -1,25 +1,38 @@
-module Impl = Libc_aarch64_apple_darwin
-[@@config all (target_os = "macos", target_arch = "aarch64")]
+include Libc_aarch64_apple_darwin [@@config
+                                    all
+                                      ( target_os = "macos",
+                                        target_arch = "aarch64" )]
 
-module Impl = Libc_aarch64_apple_darwin
-[@@config all (target_os = "macos", target_arch = "x86_64")]
+include Libc_aarch64_apple_darwin [@@config
+                                    all
+                                      ( target_os = "macos",
+                                        target_arch = "x86_64" )]
 
-module Impl = Libc_x86_64_unknown_freebsd
-[@@config all (target_os = "freebsd", target_arch = "x86_64")]
+include Libc_x86_64_unknown_freebsd [@@config
+                                      all
+                                        ( target_os = "freebsd",
+                                          target_arch = "x86_64" )]
 
-module Impl = Libc_x86_64_linux_gnu
-[@@config all (target_os = "linux", target_arch = "x86_64", target_env = "gnu")]
+include Libc_x86_64_linux_gnu [@@config
+                                all
+                                  ( target_os = "linux",
+                                    target_arch = "x86_64",
+                                    target_env = "gnu" )]
 
-module Impl = Libc_x86_64_linux_musl
-[@@config
-  all (target_os = "linux", target_arch = "x86_64", target_env = "musl")]
+include Libc_x86_64_linux_musl [@@config
+                                 all
+                                   ( target_os = "linux",
+                                     target_arch = "x86_64",
+                                     target_env = "musl" )]
 
-module Impl = Libc_aarch64_linux_gnu
-[@@config
-  all (target_os = "linux", target_arch = "aarch64", target_env = "gnu")]
+include Libc_aarch64_linux_gnu [@@config
+                                 all
+                                   ( target_os = "linux",
+                                     target_arch = "aarch64",
+                                     target_env = "gnu" )]
 
-module Impl = Libc_aarch64_linux_musl
-[@@config
-  all (target_os = "linux", target_arch = "aarch64", target_env = "musl")]
-
-include Impl
+include Libc_aarch64_linux_musl [@@config
+                                  all
+                                    ( target_os = "linux",
+                                      target_arch = "aarch64",
+                                      target_env = "musl" )]
